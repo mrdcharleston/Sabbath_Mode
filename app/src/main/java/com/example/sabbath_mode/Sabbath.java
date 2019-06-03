85b3f355de92e7fee48de53ac864b023bd469b2e
thecodersmacbookpro <mrdcharleston@gmail.com>
2019-06-02 19:46:08 -0700

    Added user interface and string url

---
+++ app/src/main/java/com/example/sabbath_mode/Sabbath.java
@@ -0,0 +1,17 @@
+package com.example.sabbath_mode;
+
+public class Sabbath implements SabbathSystem {
+
+   private String url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today";
+
+    @Override
+    public void turnOnSabbathMode() {
+        // Checks for pre condition
+    }
+
+    
+
+
+
+
+}
---
+++ app/src/main/java/com/example/sabbath_mode/SabbathSystem.java
@@ -0,0 +1,7 @@
+package com.example.sabbath_mode;
+
+public interface SabbathSystem {
+
+    void turnOnSabbathMode();
+
+}
